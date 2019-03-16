a7388c1e697442a609001c0f519d1205487a0199
Nikita <colmakov.nikita@mail.ru>
2019-03-16 23:28:35 +0300

    completed TrainHelper

--- Transport/src/main/java/train/Train.java
+++ Transport/src/main/java/train/Train.java
@@ -8,8 +8,8 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Train implements PassengerTrain {
-    public Locomotive locomotive;
-    List<Wagon> wagons = new ArrayList<Wagon>();
+    private Locomotive locomotive;
+    private List<Wagon> wagons = new ArrayList<Wagon>();
     private int commonPassengerAmount;
     private int commonLuggageAmount;
 
@@ -18,30 +18,7 @@ public class Train implements PassengerTrain {
         this.locomotive = locomotive;
     }
 
-
-    public static void createPassengerTrain() {
-
-    }
-
-
-
     public List<Wagon> getWagons() {
         return wagons;
-    }
-
-    public List<Wagon> findByPassengerAmount(int minRange, int maxRange, List<Wagon> list4FendedWagons) {
-
-        if (minRange >= 0 && maxRange >= 1) {
-            for (Wagon wagon : wagons) {
-                if (wagon.getAmountOfPassengers() >= minRange && wagon.getAmountOfPassengers() <= maxRange) {
-                    list4FendedWagons.add(wagon);
-                }
-            }
-        } else {
-            throw new IllegalArgumentException("Illegal type of values! Range must be at least from 0 to 1");
-        }
-
-        return list4FendedWagons;
-
     }
 }
--- Transport/src/main/java/train/TrainHelper.java
+++ Transport/src/main/java/train/TrainHelper.java
@@ -2,6 +2,8 @@ package train;
 
 import interfaces.Wagon;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class TrainHelper {
@@ -37,4 +39,38 @@ public class TrainHelper {
             throw new RuntimeException("train doesn't contain any wagons!");
         }
     }
+
+    public List<Wagon> findByPassengerAmount(int minRange, int maxRange, List<Wagon> list4FendedWagons,List<Wagon> wagons) {
+
+        if (minRange >= 0 && maxRange >= 1) {
+            for (Wagon wagon : wagons) {
+                if (wagon.getAmountOfPassengers() >= minRange && wagon.getAmountOfPassengers() <= maxRange) {
+                    list4FendedWagons.add(wagon);
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Illegal type of values! Range must be at least from 0 to 1");
+        }
+
+        return list4FendedWagons;
+
+    }
+
+    public static void sortWagonsByConvenience(List<Wagon> wagons) {
+        Collections.sort(wagons, new Comparator<Wagon>() {
+
+            public int compare(Wagon o1, Wagon o2) {
+                int var1 = o1.getConvenience();
+                int var2 = o2.getConvenience();
+                if (var1 < var2){
+                    return 1;
+                }
+                else if (var1 > var2){
+                    return -1;
+                }
+                else return 0;
+            }
+        });
+    }
+
 }
